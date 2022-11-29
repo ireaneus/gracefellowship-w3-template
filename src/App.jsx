1c852f472cf1ee35a547484935281a16de00785d
ireaneus <bwdave@protonmail.com>
2022-11-29 16:47:20 -0700

    5 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
 
+const title = 'Grace Fellowship';
+const subtitle = 'of Mountain Home, Idaho';
+const abutton = () => (
+  <a className="w3-bar-item w3-button w3-hover-slategray w3-round-large" />
+);
+
 function App() {
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className="w3-top">
+      <div className="w3-bar w3-theme w3-wide w3-padding w3-card">
+        <a
+          href="#home"
+          className="w3-bar-item w3-button w3-hover-slategray w3-round-large"
+        >
+          {title} {subtitle}
+        </a>
+
+        <div className="w3-right w3-hide-small">
+          <a
+            href="#projects"
+            className="w3-bar-item w3-button w3-hover-slategray w3-round-large"
+          >
+            Sermons
+          </a>
+          <a
+            href="#about"
+            className="w3-bar-item w3-button w3-hover-slategray w3-round-large"
+          >
+            About
+          </a>
+          <a
+            href="#contact"
+            className="w3-bar-item w3-button w3-hover-slategray w3-round-large"
+          >
+            Contact
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import 'normalize.css';
+import './styles/styles.scss';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
