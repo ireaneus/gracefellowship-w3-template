ca4524f7c4502acd9c122a364223b33946bb89b0
ireaneus <bwdave@protonmail.com>
2022-12-07 14:15:17 -0700

    1 file modified

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 export default function About() {
   return (
     <>
-      <div className="w3-container w3-padding-32" id="about">
-        <h3 className="w3-border-bottom w3-border-light-grey w3-padding-16">
+      <div className="w3-container w3-padding-32">
+        <h3
+          className="w3-border-bottom w3-border-light-grey w3-padding-16"
+          id="about"
+        >
           About
         </h3>
         <p>
