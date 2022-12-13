971dae5c558b80f62ca14c335ede943b942c87ec
ireaneus <bwdave@protonmail.com>
2022-12-13 08:50:34 -0700

    Got the church images in the header, images are ok

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { title, subtitle } from '../../App';
 
 const churchImgUrl = [
-  '/images/pexels-matt-hardy-2602543.jpg',
+  '/images/Church300.jpg',
+  '/images/churchus.jpg',
+  '/images/Pastor_2.jpg',
 ];
 
 function churchNum() {
@@ -13,7 +15,7 @@ export default function Header() {
   console.log(churchImgUrl);
   return (
     <header className="w3-display-container w3-wide" id="home">
-      <img src={churchImgUrl} alt={churchImgUrl} height="500px" />
+      <img src={churchImgUrl[churchNum()]} alt={churchImgUrl} height="500px" />
       <div className="w3-display-middle w3-margin-top w3-center">
         <h1 className="w3-large w3-text-white">
           <span className="w3-padding w3-theme-d4 w3-opacity-min">
